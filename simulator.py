cf773b67a9112caa1eaf7064220e48a7a6ce7ad8
rayan ali <rayan@rayans-MBP.attlocal.net>
2024-04-22 16:20:02 -0500

    all files

--- simulator.py
+++ simulator.py
@@ -1,6 +1,7 @@
 import cache
 import random
 import statistics
+from tabulate import tabulate
 
 class Simulator:
     def __init__(self, assoc):
@@ -26,7 +27,6 @@ class Simulator:
     
     def simulate(self, file_name):
         time = 0
-        async_time = 0
         L1_try = 0
         L2_try = 0
         L1_rw = 0
@@ -98,9 +98,8 @@ class Simulator:
                             time += 5
                             idx = random.randint(0, 3)
                             self.ran += 1
-                            # *** figure out write timing
                             '''if self.L2.data[index_L2][idx].dirty:
-                                print("GOt")'''
+                                print("WRITE BACK TO MEMORY")'''
                             self.L2.data[index_L2][idx].tag = tag_L2
                             self.L2.data[index_L2][idx].bytes = value
                             self.L2.data[index_L2][idx].dirty = False
@@ -114,7 +113,7 @@ class Simulator:
                     L1_try += 1
                     L1_rw += 1
                     if self.L1.data[index_L1].tag == tag_L1:
-                        async_time = max(async_time, time + 5)
+                        time += 5
                         L2_rw += 1
                         gotHit = True
                         L1_hit += 1
@@ -134,7 +133,7 @@ class Simulator:
                         L2_rw += 1
                         for cLine in self.L2.data[index_L2]:
                             if cLine.tag == tag_L2:
-                                async_time = max(async_time, time + 5)
+                                time += 5
                                 gotHit = True
                                 L2_hit += 1
                                 penalty += 5
@@ -234,12 +233,14 @@ class Simulator:
                 elif op == '4':
                     print("IGNORE")
 
-        return L1_try, L1_hit, L2_try, L2_hit, L1_rw, L2_rw, DRAM_rw, max(time, async_time)
+        return L1_try, L1_hit, L2_try, L2_hit, L1_rw, L2_rw, DRAM_rw, time, penalty
 
 
 if __name__ == "__main__":
-    file_names = ['008.espresso.din', '013.spice2g6.din', '022.li.din', '026.compress.din', '048.ora.din', '085.gcc.din']
+    file_names = ['008.espresso.din', '013.spice2g6.din', '015.doduc.din', '022.li.din', '023.eqntott.din', '026.compress.din', '034.mdljdp2.din', '039.wave5.din', '047.tomcatv.din', '048.ora.din', '085.gcc.din', '089.su2cor.din', '090.hydro2d.din', '093.nasa7.din', '094.fpppp.din']
+    table_data = []
     for name in file_names:
+        table = []
         print("-------------------------------------------")
         print(name.upper())
         print()
@@ -251,36 +252,71 @@ if __name__ == "__main__":
         L1ActiveEnergy = []
         L2ActiveEnergy = []
         DRAMActiveEnergy = []
-        for i in range(1, 11):
-            print(i,"/10")
+        penalties = []
+        times = []
+        # run trace ten times
+        for _ in range(10):
             sim = Simulator(4)
-            L1_try, L1_hit, L2_try, L2_hit, L1_rw, L2_rw, DRAM_rw, time = sim.simulate(name)
+            L1_try, L1_hit, L2_try, L2_hit, L1_rw, L2_rw, DRAM_rw, time, penalty = sim.simulate(name)
             L1HitRate.append(L1_hit/L1_try)
             L2HitRate.append(L2_hit/L2_try)
 
             L1_active_time = L1_rw * 0.5
             L1ActiveEnergy.append(L1_active_time * 1)
-            L1_static_time = time - L1_active_time
+            L1_static_time = max(time - L1_active_time, 0)
             L1StaticEnergy.append(L1_static_time * 0.5)
 
             L2_active_time = L2_rw * 4.5
             L2ActiveEnergy.append(L2_active_time * 2)
-            L2_static_time = time - L2_active_time
+            L2_static_time = max(time - L2_active_time, 0)
             L2StaticEnergy.append(L2_static_time * 0.8)
 
             DRAM_active_time = DRAM_rw * 45
             DRAMActiveEnergy.append(DRAM_active_time * 4)
-            DRAM_static_time = time - DRAM_active_time
+            DRAM_static_time = max(time - DRAM_active_time, 0)
             DRAMStaticEnergy.append(DRAM_static_time * 0.8)
 
-            print(time)
+            penalties.append(penalty)
+            times.append(time)
 
+        print("Avg time: ", statistics.mean(times), "ns","-------- Std Dev: ", statistics.stdev(times))
+        print()
         print("Avg L1 Hit Rate: ", statistics.mean(L1HitRate),"-------- Std Dev: ", statistics.stdev(L1HitRate))
         print("Avg L2 Hit Rate: ", statistics.mean(L2HitRate),"-------- Std Dev: ", statistics.stdev(L2HitRate))
         print()
-        print("Avg L1 Active Energy: ", statistics.mean(L1ActiveEnergy),"-------- Std Dev: ", statistics.stdev(L1ActiveEnergy))
-        print("Avg L1 Static Energy: ", statistics.mean(L1StaticEnergy),"-------- Std Dev: ", statistics.stdev(L1StaticEnergy))
+        print("Avg L1 Active Energy: ", statistics.mean(L1ActiveEnergy), "W","-------- Std Dev: ", statistics.stdev(L1ActiveEnergy))
+        print("Avg L1 Static Energy: ", statistics.mean(L1StaticEnergy), "W","-------- Std Dev: ", statistics.stdev(L1StaticEnergy))
         print()
-        print("Avg L2 Active Energy: ", statistics.mean(L2ActiveEnergy),"-------- Std Dev: ", statistics.stdev(L2ActiveEnergy))
-        print("Avg L2 Static Energy: ", statistics.mean(L2StaticEnergy),"-------- Std Dev: ", statistics.stdev(L2StaticEnergy))
+        print("Avg L2 Active Energy: ", statistics.mean(L2ActiveEnergy), "W","-------- Std Dev: ", statistics.stdev(L2ActiveEnergy))
+        print("Avg L2 Static Energy: ", statistics.mean(L2StaticEnergy), "W","-------- Std Dev: ", statistics.stdev(L2StaticEnergy))
         print()
+        print("Avg DRAM Active Energy: ", statistics.mean(DRAMActiveEnergy), "W","-------- Std Dev: ", statistics.stdev(DRAMActiveEnergy))
+        print("Avg DRAM Static Energy: ", statistics.mean(DRAMStaticEnergy), "W","-------- Std Dev: ", statistics.stdev(DRAMStaticEnergy))
+        print()
+        print("Avg Penalty: ", statistics.mean(penalties), "pJ","-------- Std Dev: ", statistics.stdev(penalties))
+        table.append(name.upper())
+        table.append(statistics.mean(times))
+        table.append(statistics.stdev(times))
+        table.append(statistics.mean(L1ActiveEnergy))
+        table.append(statistics.stdev(L1ActiveEnergy))
+        table.append(statistics.mean(L1StaticEnergy))
+        table.append(statistics.stdev(L1StaticEnergy))
+        table.append(statistics.mean(L2ActiveEnergy))
+        table.append(statistics.stdev(L2ActiveEnergy))
+        table.append(statistics.mean(L2StaticEnergy))
+        table.append(statistics.stdev(L2StaticEnergy))
+        table.append(statistics.mean(DRAMActiveEnergy))
+        table.append(statistics.stdev(DRAMActiveEnergy))
+        table.append(statistics.mean(DRAMStaticEnergy))
+        table.append(statistics.stdev(DRAMStaticEnergy))
+        table.append(statistics.mean(penalties))
+        table.append(statistics.stdev(penalties))
+        table_data.append(table)
+
+    print(tabulate(table_data, headers=["File Name", "Avg Time", "StdDev Time", "Avg L1 Active (W)", "StdDev L1 Active", "Avg L1 Static (W)", "StDev L1 Static", "Avg L2 Active (W)", "StdDev L2 Active", "Avg L2 Static (W)", "StDev L2 Static", "Avg DRAM Active (W)", "StdDev DRAM Active", "Avg DRAM Static(W)", "StDev DRAM Static", "Avg Penalty (jP)", "StdDev Penalty"]))
+
+    
+
+
+
+
