758f74933e3d38abe647c1b7989fbac9f2ae8633
rayan ali <rayan@rayans-MacBook-Pro.local>
2024-04-21 19:48:55 -0500

    close need to add penalties

--- simulator.py
+++ simulator.py
@@ -27,6 +27,8 @@ class Simulator:
     def simulate(self, file_name):
         time = 0
         async_time = 0
+        L1_try = 0
+        L2_try = 0
         L1_rw = 0
         L2_rw = 0
         DRAM_rw = 0
@@ -51,6 +53,7 @@ class Simulator:
                 if op == '0':
                     gotHit = False
                     # try L1 cache
+                    L1_try += 1
                     L1_rw += 1
                     if self.L1.data[index_L1].tag == tag_L1:
                         time += 0.5
@@ -59,13 +62,14 @@ class Simulator:
 
                     # try L2 cache
                     if not gotHit:
+                        L2_try += 1
                         L2_rw += 1
                         for cLine in self.L2.data[index_L2]:
                             if cLine.tag == tag_L2:
                                 time += 5
+                                penalty += 5
                                 gotHit = True
                                 L2_hit += 1
-                                # *** figure out write timing
 
                                 # move cache line up to L1
                                 self.L1.data[index_L1].tag = tag_L1
@@ -76,6 +80,7 @@ class Simulator:
                     # have to go to DRAM & store in L2 and L1
                     if not gotHit:
                         time += 50
+                        penalty += 5 + 640
                         DRAM_rw += 1
                         stored = False
                         for cLine in self.L2.data[index_L2]:
@@ -106,16 +111,18 @@ class Simulator:
                 elif op == '1':
                     gotHit = False
                     # try L1 cache
+                    L1_try += 1
                     L1_rw += 1
                     if self.L1.data[index_L1].tag == tag_L1:
-                        time += 5
+                        async_time = max(async_time, time + 5)
+                        L2_rw += 1
                         gotHit = True
                         L1_hit += 1
                         self.L1.data[index_L1].bytes = value
                         self.L1.data[index_L1].dirty = True
                         # write through to L2 cache
                         for cLine in self.L2.data[index_L2]:
-                            # check for empty
+                            # check for match
                             if cLine.tag == tag_L2:
                                 cLine.bytes = value
                                 cLine.dirty = True
@@ -123,12 +130,14 @@ class Simulator:
 
                     # try L2 cache
                     if not gotHit:
+                        L2_try += 1
                         L2_rw += 1
                         for cLine in self.L2.data[index_L2]:
                             if cLine.tag == tag_L2:
-                                time += 5
+                                async_time = max(async_time, time + 5)
                                 gotHit = True
                                 L2_hit += 1
+                                penalty += 5
 
                                 cLine.bytes = value
                                 cLine.dirty = True
@@ -139,9 +148,8 @@ class Simulator:
 
                     # have to go to DRAM & store in L2
                     if not gotHit:
-                        time += 50
-                        DRAM_rw += 1
                         stored = False
+                        penalty += 5 + 640
                         for cLine in self.L2.data[index_L2]:
                             # check for empty
                             if cLine.tag == "":
@@ -168,6 +176,7 @@ class Simulator:
                 elif op == '2':
                     gotHit = False
                     # try L1 cache
+                    L1_try += 1
                     L1_rw += 1
                     if self.L1.instructions[index_L1].tag == tag_L1:
                         time += 0.5
@@ -176,13 +185,14 @@ class Simulator:
                     
                     # try L2 cache
                     if not gotHit:
+                        L2_try += 1
                         L2_rw += 1
                         for cLine in self.L2.data[index_L2]:
                             if cLine.tag == tag_L2:
                                 time += 5
+                                penalty += 5
                                 gotHit = True
                                 L2_hit += 1
-                                # *** figure out write timing
 
                                 # move cache line up to L1
                                 self.L1.instructions[index_L1].tag = tag_L1
@@ -194,6 +204,7 @@ class Simulator:
                     if not gotHit:
                         time += 50
                         DRAM_rw += 1
+                        penalty += 5 + 640
                         stored = False
                         for cLine in self.L2.data[index_L2]:
                             # check for empty
@@ -222,41 +233,54 @@ class Simulator:
 
                 elif op == '4':
                     print("IGNORE")
-                    
-        print(time)
-        L1_idle = (time - 0.5 * L1_rw)/0.5
-        L1_energy = L1_idle * 0.5 + L1_rw * 1
-        L2_idle = (time - 5 * L2_rw)/5
-        L2_energy = L2_idle * 0.8 + L2_rw * 2
-        DRAM_idle = (time - 50 * L1_rw)/50
-        DRAM_energy = DRAM_idle * 0.8 + DRAM_rw * 4
 
-        '''print(L1_energy + L2_energy + DRAM_energy)
-        print("L1 COUNT: ", L1_rw)
-        print("L2 COUNT: ", L2_rw)
-        print("DRAM COUNT: ", DRAM_rw)
-        print("RANDOM: ", self.ran)
-        print("L1 HIT RATE: ", L1_hit/L1_rw)
-        print("L2 HIT: ", L2_hit/L2_rw)'''
-
-        return L1_rw, L1_hit, L2_rw, L2_hit, DRAM_rw, time
+        return L1_try, L1_hit, L2_try, L2_hit, L1_rw, L2_rw, DRAM_rw, max(time, async_time)
 
 
 if __name__ == "__main__":
-    #file_names = ['008.espresso.din', '013.spice2g6.din', '022.li.din', '026.compress.din', '048.ora.din', '085.gcc.din']
-    file_names = ['026.compress.din']
+    file_names = ['008.espresso.din', '013.spice2g6.din', '022.li.din', '026.compress.din', '048.ora.din', '085.gcc.din']
     for name in file_names:
         print("-------------------------------------------")
         print(name.upper())
         print()
         L1HitRate = []
         L2HitRate = []
-        for _ in range(10):
+        L1StaticEnergy = []
+        L2StaticEnergy = []
+        DRAMStaticEnergy = []
+        L1ActiveEnergy = []
+        L2ActiveEnergy = []
+        DRAMActiveEnergy = []
+        for i in range(1, 11):
+            print(i,"/10")
             sim = Simulator(4)
-            L1_rw, L1_hit, L2_rw, L2_hit, DRAM_rw, time = sim.simulate(name)
-            L1HitRate.append(L1_hit/L1_rw)
-            L2HitRate.append(L2_hit/L2_rw)
+            L1_try, L1_hit, L2_try, L2_hit, L1_rw, L2_rw, DRAM_rw, time = sim.simulate(name)
+            L1HitRate.append(L1_hit/L1_try)
+            L2HitRate.append(L2_hit/L2_try)
 
-        print("Avg L1 Hit Rate: ", statistics.mean(L1HitRate))
-        print("Std L1 Hit Rate: ", statistics.stdev(L1HitRate))
+            L1_active_time = L1_rw * 0.5
+            L1ActiveEnergy.append(L1_active_time * 1)
+            L1_static_time = time - L1_active_time
+            L1StaticEnergy.append(L1_static_time * 0.5)
+
+            L2_active_time = L2_rw * 4.5
+            L2ActiveEnergy.append(L2_active_time * 2)
+            L2_static_time = time - L2_active_time
+            L2StaticEnergy.append(L2_static_time * 0.8)
+
+            DRAM_active_time = DRAM_rw * 45
+            DRAMActiveEnergy.append(DRAM_active_time * 4)
+            DRAM_static_time = time - DRAM_active_time
+            DRAMStaticEnergy.append(DRAM_static_time * 0.8)
+
+            print(time)
+
+        print("Avg L1 Hit Rate: ", statistics.mean(L1HitRate),"-------- Std Dev: ", statistics.stdev(L1HitRate))
+        print("Avg L2 Hit Rate: ", statistics.mean(L2HitRate),"-------- Std Dev: ", statistics.stdev(L2HitRate))
+        print()
+        print("Avg L1 Active Energy: ", statistics.mean(L1ActiveEnergy),"-------- Std Dev: ", statistics.stdev(L1ActiveEnergy))
+        print("Avg L1 Static Energy: ", statistics.mean(L1StaticEnergy),"-------- Std Dev: ", statistics.stdev(L1StaticEnergy))
+        print()
+        print("Avg L2 Active Energy: ", statistics.mean(L2ActiveEnergy),"-------- Std Dev: ", statistics.stdev(L2ActiveEnergy))
+        print("Avg L2 Static Energy: ", statistics.mean(L2StaticEnergy),"-------- Std Dev: ", statistics.stdev(L2StaticEnergy))
         print()
